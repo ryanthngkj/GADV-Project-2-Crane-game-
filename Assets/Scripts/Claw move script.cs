5788cb0bd30c5b65b9f8f28722e966293ba880b5
ryanthngkj <ryanthngkj@gmail.com>
2026-07-06 02:29:30 +0800

    Claw movement success
    The claw script is successfully working and im saving it now before it somehow breaks later on

--- Assets/Scripts/Claw move script.cs
+++ Assets/Scripts/Claw move script.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Clawmovescript : MonoBehaviour
 {
+    private Rigidbody2D clawBody;
+    private Vector2 clawMove;
+    public float speed = 40f; // Allows speed to be changed from the side menu
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        clawBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)) {    // Claw go left when press A
+            clawMove = Vector2.left; 
+        }
+        else if (Input.GetKey(KeyCode.D)){  // Claw go right when press D
+            clawMove = Vector2.right;
+        } 
+        else {
+                clawMove = Vector2.zero;
+        }
+    }
+
+    void FixedUpdate(){
+        if (clawMove == Vector2.zero) { // If no input, claw stop moving
+            return;
+        }
+        clawBody.AddForce (clawMove * speed); // Makes it so claw accelerates a little
     }
 }
+
+
+
